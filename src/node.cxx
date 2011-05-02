f6fe535939a5902cc18979256cfb04b711e716cb
Benoit Gschwind <gschwind@gnu-log.net>
2011-05-02 10:52:18 +0200

    Fix Copyright.

--- src/node.cxx
+++ src/node.cxx
@@ -1,8 +1,21 @@
 /*
- * node.cxx
+ * Copyright 2010 Benoit Gschwind <gschwind@gnu-log.net>
  *
- *  Created on: 1 mai 2011
- *      Author: gschwind
+ * This file is part of libbml.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 #include "node.hxx"
